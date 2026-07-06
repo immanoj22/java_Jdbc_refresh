9e3e91cb18cc7ee3c472f8d197535c06a449223b
manoj <158544348+satheeshmaticz@users.noreply.github.com>
2026-07-06 09:12:14 +0530

    jdbc-modiefied

--- Jdbc_Course/src/main/java/org/example/JDBC/JDBC01.java
+++ Jdbc_Course/src/main/java/org/example/JDBC/JDBC01.java
@@ -1,18 +1,140 @@
 package org.example.JDBC;
 import java.sql.*;
 public class JDBC01 {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws SQLException {
+//        StateInsert();
+//        UpdateStatement();
+//        retriveDate();
+        deleteData();
+    }
 
-        //register driver
-        Class.forName("org.postgresql.Driver");
+    public static void StateInsert() throws SQLException {
 
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user="postgres";
-        String password="Test@123";
+        Connection conect=null;
+        Statement statment=null;
+        try{
+            //register driver
+            Class.forName("org.postgresql.Driver");
 
-        //establish connection
-        Connection conect=DriverManager.getConnection(url,user,password);
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user="postgres";
+            String password="Test@123";
 
-        Statement statment=conect.createStatement();
+            //establish connection
+            conect=DriverManager.getConnection(url,user,password);
+
+            //create statement
+            statment=conect.createStatement();
+
+            //execute query
+            String sql="INSERT INTO studentInfo(id,name,age,city) VALUES (2,'ani',1,'mdu')";
+            int result=statment.executeUpdate(sql);
+            //process result
+            if(result==0){
+                System.out.println("no rows affected");
+            }else{
+                System.out.println("process success");
+            }
+
+
+        }catch(Exception E){
+            E.getStackTrace();
+        }finally {
+            //close connection
+            statment.close();
+            conect.close();
+        }
+
+    }
+
+    public static void  UpdateStatement ()throws SQLException{
+        Connection connect=null;
+        Statement statement=null;
+        try {
+            Class.forName("org.postgresql.Driver");
+
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user="postgres";
+            String password="Test@123";
+
+            connect=DriverManager.getConnection(url,user,password);
+
+            statement=connect.createStatement();
+
+            String sql="UPDATE studentinfo set age=10,id=3 WHERE name='ani' ";
+            int affectedRows=statement.executeUpdate(sql);
+
+            if(affectedRows==0){
+                System.out.println("no rows affected");
+            }else{
+                System.out.println("proccess success");
+            }
+        }catch (Exception E){
+            E.printStackTrace();
+        }finally {
+            connect.close();
+            statement.close();
+        }
+    }
+
+    public static void retriveDate() throws SQLException{
+        Connection connection=null;
+        Statement statement=null;
+        try{
+            Class.forName("org.postgresql.Driver");
+
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user="postgres";
+            String password="Test@123";
+
+            connection=DriverManager.getConnection(url,user,password);
+
+            statement=connection.createStatement();
+
+            String sql="SELECT * FROM studentInfo";
+            ResultSet result=statement.executeQuery(sql);
+
+            while (result.next()){
+                System.out.println(result.getInt(1)+" "+result.getString(2)+" "+result.getInt(3)+" "+result.getString(4));
+            }
+            result.close();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            statement.close();
+            connection.close();
+        }
+    }
+
+    public static void deleteData() throws SQLException{
+        Connection connection=null;
+        Statement statement=null;
+        try{
+            Class.forName("org.postgresql.Driver");
+
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            String user="postgres";
+            String password="Test@123";
+
+            connection=DriverManager.getConnection(url,user,password);
+
+            statement=connection.createStatement();
+
+            String sql="DELETE FROM studentinfo WHERE id=2";
+            int success=statement.executeUpdate(sql);
+            if(success!=0){
+                System.out.println("process success");
+            }else{
+                System.out.println("proccess failed");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            statement.close();
+            connection.close();
+        }
     }
 }
